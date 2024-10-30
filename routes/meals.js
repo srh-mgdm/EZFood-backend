b60b81f9236934ccd0579a53139a99b08f0a41cd
EverAsunder <ever.asunder@gmail.com>
2024-10-30 14:06:22 +0100

    search meal based on search index

--- routes/meals.js
+++ routes/meals.js
@@ -20,12 +20,39 @@ router.get("/all", function (req, res) {
 
 /* GET meal by meal name - token NOT required */
 router.get("/name/:mealName", function (req, res) {
-  // Let's use ranked search patterns
-  Meals.find(
-    { $text: { $search: req.params.mealName } },
-    { score: { $meta: "textScore" } }
-  )
-    .sort({ score: { $meta: "textScore" } })
+  // Let's use aggregation framework
+  Meals.aggregate([
+    {
+      $search: {
+        index: "default", // search index created in the db
+        compound: {
+          should: [
+            {
+              autocomplete: {
+                query: req.params.mealName, // text to search
+                path: "mealName", // field to search in the db
+                fuzzy: {
+                  maxEdits: 2, // tolerance for typing errors
+                },
+                score: { boost: { value: 20 } }, // boost results matching the text
+              },
+            },
+          ],
+        },
+      },
+    },
+    {
+      $project: {
+        mealName: 1,
+        score: { $meta: "searchScore" },
+      },
+    },
+    {
+      $sort: {
+        score: -1, // sort by score in descending order
+      },
+    },
+  ])
     .then((results) => {
       res.json({ result: true, meals: results });
     })
