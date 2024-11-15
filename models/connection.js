f575fc739d67cf53ba9df33da5eafa779bcd188e
Sareh <moghaddam_sareh@yahoo.com>
2024-11-15 22:07:52 +0100

    add README.md

--- models/connection.js
+++ models/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-connectionString = process.env.CONNECTION_STRING + "ezfood";
+connectionString = process.env.CONNECTION_STRING;
 mongoose
   .connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log("connected to ezfood db"))
